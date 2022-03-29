8f26e1a51ee4cb6b9e41f7af42f27a897937a775
esinimutlu <esinimutluuu@gmail.com>
2022-03-29 14:20:41 +0300

    mdv61850-app-notes updated

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -46,6 +46,7 @@ module.exports = {
           "SNMP-Application-Notes",
 		  "DNP3-Application-Notes",
 		  "Block-Descriptions-Notes",
+		  "MDV61850BROWSER-Application-Notes",
     ],
     },
      
